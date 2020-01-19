358513ae017c114b80327cf7fa9ff5521258f258
Giles Lloyd <giles.af.lloys@gmail.com>
2020-01-19 10:42:59 +0000

    feat(infrastructure): Composer install / DB setup

--- docker/postgresql/init.sh
+++ docker/postgresql/init.sh
@@ -3,8 +3,8 @@ set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
     ALTER USER postgres PASSWORD 'password';
-    GRANT ALL PRIVILEGES ON DATABASE fmf-auth TO fmf-auth;
+    GRANT ALL PRIVILEGES ON DATABASE wallys TO wally;
 
 EOSQL
 
-psql fmf-auth -c 'create extension hstore;'
+psql wallys -c 'create extension hstore;'
